940dfe590f53e19edfe0ef6fd3dd429c26bc8795
MinayeAliyeva <7s346pu@code.edu.az>
2024-03-05 21:23:56 +0400

    changes

--- callback.js
+++ callback.js
@@ -11,11 +11,55 @@
 // };
 // sayHello(() => console.log("Hello"));
 // jsonplaceholder
-fetch("https://jsonplaceholder.typicode.com/users")
-  .then((data) => data.json())
-  .then(
-    (posts) => console.log("users"),
-    fetch("https://jsonplaceholder.typicode.com/posts/1")
-      .then((data) => data.json())
-      .then((users) => console.log("posts"))
-  );
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((data) => data.json())
+//   .then(
+//     (posts) => console.log("1",posts),
+//     fetch("https://jsonplaceholder.typicode.com/posts/2")
+//       .then((data) => data.json())
+//       .then(
+//         (users) => console.log("2",users),
+//         fetch("https://jsonplaceholder.typicode.com/posts/1")
+//           .then((data) => data.json())
+//           .then((post2) => console.log("3",post2))
+//       )
+//   );
+
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((data) => data.json())
+//       .then((users) => console.log("ayri users"))
+//   fetch("https://jsonplaceholder.typicode.com/posts/1")
+//       .then((data) => data.json())
+//       .then((users) => console.log("ayri posts"))
+
+// async function getData() {
+//   const users = await (
+//     await fetch("https://jsonplaceholder.typicode.com/users")
+//   ).json();
+//   const post1 = await (
+//     await fetch("https://jsonplaceholder.typicode.com/posts/1")
+//   ).json();
+//   const post2 = await (
+//     await fetch("https://jsonplaceholder.typicode.com/posts/2")
+//   ).json();
+//   console.log("users", users);
+//   console.log("post/2", post2);
+//   console.log("post/1", post1);
+// }
+
+// getData();
+
+(async () => {
+  const users = await (
+    await fetch("https://jsonplaceholder.typicode.com/users")
+  ).json();
+  const post1 = await (
+    await fetch("https://jsonplaceholder.typicode.com/posts/1")
+  ).json();
+  const post2 = await (
+    await fetch("https://jsonplaceholder.typicode.com/posts/2")
+  ).json();
+  console.log("users", users);
+  console.log("post/2", post2);
+  console.log("post/1", post1);
+})();

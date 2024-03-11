af78a94845f3b927d5cdf5bc000dfe2adeaf2848
MinayeAliyeva <7s346pu@code.edu.az>
2024-03-11 19:58:25 +0400

    end

---
+++ state/src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import { useState } from "react";
+function App() {
+  const [name, setName] = useState("Minaya");
+  const [age, setAge] = useState(20);
+  const [friends, setFriends] = useState(["Gulu", "Emin", "Fidan"]);
+  const [adress, setAdress] = useState({ title: "Istanbul", zip: 1234 });
+  return (
+    <div className="App">
+      <h1>
+        Mehraba {name} {age}
+      </h1>
+      <button onClick={() => setName("Ahmet")}>Click</button>
+      <button onClick={() => setAge(24)}>Click</button>
+      <hr />
+      <br />
+      <h2>Friends</h2>
+      {friends?.map((friend, i) => (
+        <h1 key={i}>{friend}</h1>
+      ))}
+      <br />
+      <div>
+        {adress?.title} {adress?.zip}
+      </div>
+      <button onClick={() => setAdress({...adress, title: "Ankara" })}>
+        update object
+      </button>
+      <br />
+    </div>
+  );
+}
+
+export default App;
